065cd480befd9f9c522ca46d85eb27c52dda2062
Taras <tarasostapchuk27@gmail.com>
2025-11-02 13:52:15 +0100

    Add page for worker_list

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from tasks.views import index
+from tasks.views import index, WorkerListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("workers/", WorkerListView.as_view(), name="worker-list")
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from tasks.models import Worker
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "home/index.html")
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    template_name = "home/worker_list.html"
