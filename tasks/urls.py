4820174fcb0b851d3022068869f3e432121ae781
Taras <tarasostapchuk27@gmail.com>
2025-11-13 18:08:44 +0100

    Add url for worker creation view

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,12 +7,14 @@ from tasks.views import (
     PositionListView,
     TaskListView,
     TaskDetailView,
+    WorkerCreateView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
     path("positions/", PositionListView.as_view(), name="position-list"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
