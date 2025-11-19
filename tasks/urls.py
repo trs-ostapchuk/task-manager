25a27b933f05833852535aac745f304f001ae90c
Taras <tarasostapchuk27@gmail.com>
2025-11-19 10:18:04 +0100

    Add URL for deleting worker

--- tasks/urls.py
+++ tasks/urls.py
@@ -9,6 +9,7 @@ from tasks.views import (
     TaskDetailView,
     WorkerCreateView,
     WorkerUpdateView,
+    WorkerDeleteView,
 )
 
 urlpatterns = [
@@ -17,6 +18,7 @@ urlpatterns = [
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
     path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
     path("positions/", PositionListView.as_view(), name="position-list"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
